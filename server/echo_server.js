15e5713ac75527d27d642eeba532fea768869f2d
aliakseikrauchanka <aliaksei_krauchanka@epam.com>
2018-01-14 18:49:52 +0300

    15.0 [echo-server] whiteHead write immediately into response

--- server/echo_server.js
+++ server/echo_server.js
@@ -3,8 +3,12 @@ var url = require('url');
 
 var server = new http.Server();
 server.on('request', (req, res) => {
+
     let parsedUrl = url.parse(req.url, true);
     if (parsedUrl.pathname === '/echo' && parsedUrl.query.message) {
+        // res.writeHead(200, 'OK', { 'Cache-control': 'no-cache' });
+
+        res.setHeader('Cache-control', 'no-cache');
         res.end(parsedUrl.query.message);
     } else {
         res.statusCode = 404;
