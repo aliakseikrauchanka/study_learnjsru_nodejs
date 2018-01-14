5d0df3ed1295602d90282c89b030864deb800ea2
aliakseikrauchanka <aliaksei_krauchanka@epam.com>
2018-01-14 18:28:08 +0300

    14.1 [Node.js as a server] Wrap emit function, http.Server -> net.Server -> Event Emitter

--- server/simple_server.js
+++ server/simple_server.js
@@ -5,13 +5,20 @@ var counter = 0;
 // option 1
 var server = new http.Server(); // http.Server -> net.Server -> Event Emitter
 
+var emit = server.emit;
+
+// Keep-Alive
+server.emit = function (event/*, ...args*/) {
+    console.log(event);
+    emit.apply(server, arguments);
+};
+
 var counter = 0;
-server.listen('999', 'localhost');
 server.on('request', (req, res) => {
     res.end(`Hello from first simple server: ${++counter}`);
 });
 
-// --- option 2
+// option 2
 // var server = new http.createServer((req, res) => {
 //     res.end(`Hello from first simple server: ${++counter}`);
 // }); // Event Emitter
