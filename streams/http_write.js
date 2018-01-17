ea36baa1e6df92a7b24cd9a26049952eca96d215
aliakseikrauchanka <aliaksei_krauchanka@epam.com>
2018-01-17 17:17:22 +0300

    26. [writable] Use pipe instead of read/write/drain combination

--- streams/http_write.js
+++ streams/http_write.js
@@ -5,27 +5,12 @@ http.createServer((req, res) => {
     // res: ServerResponse > stream.Writable
 
     if (req.url === '/file') {
-        var file = new fs.ReadStream(__filename, { encoding: 'utf-8' });
+        var file = new fs.ReadStream(__filename);
 
-        send(file, res);
+        file.pipe(res);
+        file.pipe(process.stdout);
     } else {
         res.end('ololo');
     }
 }).listen(777);
 
-function send(file, res) {
-    file.on('readable', write);
-    file.on('end', () => {res.end();});
-
-    function write() {
-        var data = file.read(); // READ
-        if (data && !res.write(data)) { // TRY WRITE
-            file.removeListener('readable', write);
-
-            res.once('drain', () => { // WAIT until write stream is drain
-                file.on('readable', write);
-                write();
-            });
-        }
-    }
-}
