664df057de2c081f5a6f7275e17810e823b6c096
aliakseikrauchanka <aliaksei_krauchanka@epam.com>
2018-01-17 18:24:28 +0300

    26 [writable] Switch to pipe + handle error in Readable + .destroy Readable on res "close"

--- streams/http_write.js
+++ streams/http_write.js
@@ -5,12 +5,33 @@ http.createServer((req, res) => {
     // res: ServerResponse > stream.Writable
 
     if (req.url === '/file') {
-        var file = new fs.ReadStream(__filename);
+        var file = new fs.ReadStream('big.html');
 
-        file.pipe(res);
-        file.pipe(process.stdout);
+        sendFile(file, res);
     } else {
         res.end('ololo');
     }
 }).listen(777);
+
+function sendFile(file, to) {
+    file.pipe(to);
+
+    file
+        .on('error', err => {
+            to.statusCode = 500;
+            console.log(err);
+            to.end('Error while reading');
+        })
+        .on('open', () => {
+            console.log('open');
+        })
+        .on('close', () => {
+            console.log('close');
+        });
+
+    // need to wait for 'close' res event in order to destroy file accordingly
+    to.on('close', () => {
+        file.destroy();
+    });
+}
 
