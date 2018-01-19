639c56321cac5a7ecf9e8ad8e9fd1e9e974ee174
aliakseikrauchanka <aliaksei_krauchanka@epam.com>
2018-01-19 12:46:15 +0300

    [domain] Async + read file

--- domain/domain.js
+++ domain/domain.js
@@ -1,12 +1,19 @@
-var domain = require('domain');
+const domain = require('domain');
+const fs = require('fs');
 
 var ownDomain = domain.create();
 
 ownDomain.on('error', err => {
-    console.log(`Catched error: ${err.code}`);
+    console.log(`Catched error: ${err}`);
 });
 
 ownDomain.run(() => {
-    console.log(process.domain);
-    ERROR();
+    // domain.enter();
+    setTimeout(() => {
+        fs.readFile(__filename, (err, data) => {
+            ERROR();
+        });
+        console.log(process.domain);
+    }, 1000);
+    // domain.exit();
 });
